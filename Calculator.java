e19161c7d8db47ba33393d776bff6dcc73d337f7
tasnimsiddiqa <56696590+tasnimsiddiqa@users.noreply.github.com>
2019-10-18 13:26:16 +0600

    calculator

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator{
+	public static void main(String[] args){
+
+		System.out.println(addition(3,4));
+		System.out.println(subtraction(13,4));
+		System.out.println(multiplication(5,5));
+		System.out.println(division(5,5));
+
+
+	}
+
+	double addition(double x, int double){
+		return x + y;
+	}
+
+	double subtraction(double x, double y){
+		return x - y;
+	}
+	double multiplication(double x, double y){
+		return x * y;
+	}
+	double division(double x, double y){
+		return x / y;
+	}
+}
